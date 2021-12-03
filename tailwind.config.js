0ace96f38d98e8863c353a3b3f18076a33d5a1b7
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2021-12-03 16:14:37 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./components/**/*.{js,vue}", "./src/**/*.{js,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
